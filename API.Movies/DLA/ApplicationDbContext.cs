327a01d02c574f510f9b5cfc1cc10343ea1b353d
LuisMiguelQuintero <luisqg.2012@gmail.com>
2025-11-30 22:08:17 -0500

    Add project files.

---
+++ API.Movies/DLA/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using API.Movies.DLA.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Movies.DLA
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+        
+        }
+
+        //seccion para crear el dbset de las entidades o modelos
+        public DbSet<Category> Categories { get; set; }  
+
+    }
+}
---
+++ API.Movies/DLA/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Movies.DLA.Models
+{
+    public class Category: AuditBase
+    {
+        [Required] //Indica que este tampoco es obligatorio
+        [Display(Name = "Nombre de la categoria")] // me sirve para personalizar el nombre que se muestra en las vistas
+        public string Name { get; set; }
+
+    }
+}
+
